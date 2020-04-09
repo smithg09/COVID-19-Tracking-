c9b24f52c7553f85dce94361268ceee99f723870
Smith <smith.gajajr@gmail.com>
2020-04-09 18:56:48 +0530

    New update

--- scripts/main_card.js
+++ scripts/main_card.js
@@ -11,50 +11,55 @@ $(document).ready(() => {
         var all_stat = response.countries_stat
         console.log(all_stat)
         all_stat.forEach(countries => {
-            var main_card = document.getElementById("main_card");
+            if (countries.country_name === "") {
+                
+            }
+            else {
 
-            var card_body = document.createElement("div");
-            card_body.setAttribute("class", "card-body p-0 border-top")
-            main_card.appendChild(card_body)
+                var main_card = document.getElementById("main_card");
 
-            var blog_comments = document.createElement("div");
-            blog_comments.setAttribute("class", "blog-comments__item d-flex p-3")
-            card_body.appendChild(blog_comments)
+                var card_body = document.createElement("div");
+                card_body.setAttribute("class", "card-body p-0 border-top")
+                main_card.appendChild(card_body)
 
-            var blog_content = document.createElement("div");
-            blog_content.setAttribute("class", "blog-comments__content")
-            blog_comments.appendChild(blog_content)
+                var blog_comments = document.createElement("div");
+                blog_comments.setAttribute("class", "blog-comments__item d-flex p-3")
+                card_body.appendChild(blog_comments)
 
-            var blog_meta = document.createElement("div");
-            blog_meta.setAttribute("class", "blog-comments__meta text-muted")
-            blog_content.appendChild(blog_meta)
+                var blog_content = document.createElement("div");
+                blog_content.setAttribute("class", "blog-comments__content")
+                blog_comments.appendChild(blog_content)
 
-            var a_tag = document.createElement("a");
-            a_tag.setAttribute("class", "text-secondary");
-            a_tag.setAttribute("style", "font-size:1.3rem");
-            a_tag.setAttribute("href", "#");
-            a_tag.innerText = countries.country_name
-            blog_meta.appendChild(a_tag)
+                var blog_meta = document.createElement("div");
+                blog_meta.setAttribute("class", "blog-comments__meta text-muted")
+                blog_content.appendChild(blog_meta)
 
-            var contain = document.createElement("div");
-            contain.setAttribute("class", "contain")
-            blog_content.appendChild(contain)
+                var a_tag = document.createElement("a");
+                a_tag.setAttribute("class", "text-secondary");
+                a_tag.setAttribute("style", "font-size:1.3rem");
+                a_tag.setAttribute("href", "#");
+                a_tag.innerText = countries.country_name
+                blog_meta.appendChild(a_tag)
 
-            var p1 = document.createElement("p");
-            p1.setAttribute("class", "m-0 my-1 mb-2 c_prim");
-            p1.innerText = "Total Cases: " + countries.cases
-            contain.appendChild(p1)
+                var contain = document.createElement("div");
+                contain.setAttribute("class", "contain")
+                blog_content.appendChild(contain)
 
-            var p2 = document.createElement("p");
-            p2.setAttribute("class", "m-0 my-1 mb-2 c_danger");
-            p2.innerText = "Total Deaths: " + countries.deaths 
-            contain.appendChild(p2)
+                var p1 = document.createElement("p");
+                p1.setAttribute("class", "m-0 my-1 mb-2 c_prim");
+                p1.innerText = "Total Cases: " + countries.cases
+                contain.appendChild(p1)
 
-            var p3 = document.createElement("p");
-            p3.setAttribute("class", "m-0 my-1 mb-2 c_relief");
-            p3.innerText = "Total Recovered: " + countries.total_recovered
-            contain.appendChild(p3)
+                var p2 = document.createElement("p");
+                p2.setAttribute("class", "m-0 my-1 mb-2 c_danger");
+                p2.innerText = "Total Deaths: " + countries.deaths
+                contain.appendChild(p2)
 
+                var p3 = document.createElement("p");
+                p3.setAttribute("class", "m-0 my-1 mb-2 c_relief");
+                p3.innerText = "Total Recovered: " + countries.total_recovered
+                contain.appendChild(p3)
+            }
         });
         
 
