9f2547ab15746b87aa2b6fd98a8f1447d5f22db2
Smith <smith.gajajr@gmail.com>
2020-04-14 23:34:13 +0530

    dynamic removed

--- serviceworker.js
+++ serviceworker.js
@@ -22,8 +22,6 @@ const assets = [
   "https://use.fontawesome.com/releases/v5.0.6/css/all.css",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
-  "https://newsapi.org/v2/top-headlines?country=in&apiKey=8900a00a70244f89abeeef6d0065b7b2",
-  "https://api.covid19india.org/data.json",
   "https://code.jquery.com/jquery-3.3.1.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
 ];
@@ -66,13 +64,14 @@ self.addEventListener('fetch', evt => {
     // console.log("fetch events",evt)
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request).then(fetchRes => {
-                return caches.open(dynamicCache).then(cache => {
-                    cache.put(evt.request.url, fetchRes.clone());
-                    limitCacheSize(dynamicCache , 20)
-                    return fetchRes;
-                })
-            });
+            return cacheRes || fetch(evt.request);
+            // .then(fetchRes => {
+            //     return caches.open(dynamicCache).then(cache => {
+            //         cache.put(evt.request.url, fetchRes.clone());
+            //         limitCacheSize(dynamicCache , 20)
+            //         return fetchRes;
+            //     })
+            // });
         }).catch(() => {
             if (evt.request.url.indexOf('.html') > -1) {
                 return caches.match('/fallback.html');
